6bd1886cb1306691097e9de07bc3b01efbd1bda3
Suresh17511 <suresh.chidurala17511@gmail.com>
2021-08-29 12:52:52 +0530

    added firebase-auth

---
+++ src/config/config.ts
@@ -0,0 +1,12 @@
+const config = {
+  firebase: {
+    apiKey: "AIzaSyDaqdrrPUhoHE4oL0ALcz5LQ3bSWZo0Amw",
+    authDomain: "authentication-for-weather-app.firebaseapp.com",
+    projectId: "authentication-for-weather-app",
+    storageBucket: "authentication-for-weather-app.appspot.com",
+    messagingSenderId: "54007860021",
+    appId: "1:54007860021:web:9035fac7d926c277b79a7c",
+  },
+};
+
+export default config;
